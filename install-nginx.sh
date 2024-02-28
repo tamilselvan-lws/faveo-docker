dfb2f0d503f99b92cd6bff14bfbf44729586ff2a
tamilselvan-lws <tamil.selvan@ladybirdweb.com>
2024-02-28 09:03:54 +0530

    update network

--- install-nginx.sh
+++ install-nginx.sh
@@ -287,7 +287,7 @@ fi
 
 docker network rm ${domainname}-faveo
 
-docker network create --driver=bridge --subnet=172.22.2.0/16 ${domainname}-faveo
+docker network create --driver=bridge --subnet=172.28.0.0/16 ${domainname}-faveo
 
 if [[ $? -eq 0 ]]; then
     echo " Faveo Docker Network ${domainname}-faveo Created"
